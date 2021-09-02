34be6c5bae41a625c46eb96b9877e571563c3ee8
Vineesha1996 <Vineesha1996@gmail.com>
2021-09-02 12:31:29 +0530

    Nodejs AWS Lambda funtion-serveless
    Hello node.js Lambda function fronted by API gateway that just echos the requestor's IP address

---
+++ handler.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.hello = async (event,context) => {
+  const data = event.requestContext;
+  var httpVal=data.http;
+  var IpAddress=httpVal.sourceIp;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message:'Hey Hi,This is your IP Address'+IpAddress+" It's test api",
+        sourceIp:IpAddress
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
